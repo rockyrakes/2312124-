8dc7bf6f8b3ecf269bc9862867438b3322c28602
rockyrakes <2312124@nec.edu.in>
2026-07-02 13:12:46 +0530

    Implement fetchNotifications function with mock data

--- notification-app-fe/src/api/notifications.js
+++ notification-app-fe/src/api/notifications.js
@@ -1,2 +1,34 @@
-export function fetchNotifications() {
+export async function fetchNotifications() {
+  return {
+    totalPages: 3,
+    notifications: [
+      {
+        id: 1,
+        title: "Placement Drive",
+        message: "Company is coming.",
+        type: "Placement",
+        priority: 10,
+        read: false,
+        createdAt: "2026-07-02"
+      },
+      {
+        id: 2,
+        title: "Semester Result",
+        message: "Semester 6 Result Published.",
+        type: "Result",
+        priority: 9,
+        read: true,
+        createdAt: "2026-07-01"
+      },
+      {
+        id: 3,
+        title: "Tech Event",
+        message: "Hackathon starts tomorrow.",
+        type: "Event",
+        priority: 8,
+        read: false,
+        createdAt: "2026-06-30"
+      }
+    ]
+  };
 }
